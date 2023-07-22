b0f29ea70700fa7910f9c4add499b814b8454cfb
Alex Mangust <sanya.stogny@gmail.com>
2023-07-22 20:53:34 +0800

    Написали код где, с помощью функции массив заполняется рандомными элементами, а с помощью другой функции эти элементы выводятся на экран

--- Program.cs
+++ Program.cs
@@ -1,18 +1,24 @@
-﻿int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
-int n = array.Length;
-int find = 1;
-int index = 0;
-int count = 0;
-while(index < n)
+﻿void FildArray(int[] collection)
 {
-    if(array[index] == find)
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
     {
-    Console.WriteLine(index);
-    count++;
+        collection[index] = new Random().Next(1,10);
+        index++;
     }
-    index++;
 }
-if(count > 1)
+
+void PrintArray(int[] col)
 {
-    Console.WriteLine("В массиве " + count + " одинаковых элементов.");
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
 }
+int[] array = new int[10];
+FildArray(array);
+PrintArray(array);

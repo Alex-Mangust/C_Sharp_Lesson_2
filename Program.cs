41581916decd022c94ffe56978677e0f26ff37ba
Alex Mangust <sanya.stogny@gmail.com>
2023-07-22 20:46:24 +0800

    Поиск индекса элемента массива, который указали. Вариант 1

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
-﻿int Max(int arg1, int arg2, int arg3)
+﻿int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+int n = array.Length;
+int find = 1;
+int index = 0;
+int count = 0;
+while(index < n)
 {
-    int result = arg1;
-    if(arg2>result) result = arg2;
-    if(arg3>result) result = arg3;
-    return result;
+    if(array[index] == find)
+    {
+    Console.WriteLine(index);
+    count++;
+    }
+    index++;
 }
-int [] array = {11, 211, 31, 41, 15, 61, 17, 18, 19};
-
-int max = Max(Max(array[0], array[1], array[2]),
-Max(array[3], array[4], array[5]),
-Max(array[6], array[7], array[8]));
-Console.WriteLine(max);
+if(count > 1)
+{
+    Console.WriteLine("В массиве " + count + " одинаковых элементов.");
+}
